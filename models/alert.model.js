cc21ad935fbbe8abb52e53f16328c3a52a81805c
ivan de jesus martinez delfin <ivanmartinez_delfin@outlook.com>
2025-06-18 18:18:04 -0600

    se eliminaron carpetas duplicadas y se agrego express-validator

--- models/alert.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const alertSchema = new Schema({
-    symbol: { type: String, required: true },
-    targetPrice: { type: Number, required: true },
-    email: { type: String, required: true },
-});
-
-export default model('Alert', alertSchema); 
--- utils/cron.js
+++
@@ -1,4 +0,0 @@
-import cron from 'node-cron';
-import { checkStockPrices } from '../services/stock.services.js';
-
-cron.schedule('*/15 * * * *', checkStockPrices); //Cada 15 minutos
